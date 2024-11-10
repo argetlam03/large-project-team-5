8338ca19eb055c76b05ea0ce8f75c36eed6e6c29
argetlam03 <marrislucas@gmail.com>
2024-11-10 22:19:43 +0000

    login and createUser endpoints

--- server.js
+++ server.js
@@ -20,3 +20,72 @@ app.use((req, res, next) => {
 });
 
 app.listen(5000);
+
+// TODO: Database Implementation
+// const MongoClient = require('mongodb').MongoClient;
+// const url = '';
+// const client = new MongoClient(url);
+// client.connect();
+
+app.post('/api/login', async (req, res, next) => {
+    var error = '';
+    const { login, password } = req.body;
+    var id = -1;
+    var fn = '';
+    var ln = '';
+
+    // TODO: Database Implementation
+    // try {
+    //     const db = client.db();
+    //     const results = await db.collection('Users').find({ Login: login, Password: password }).toArray();
+
+    //     if (results.length > 0) {
+    //         id = results[0].UserId;
+    //         fn = results[0].FirstName;
+    //         ln = results[0].LastName;
+    //     }
+    //     else {
+    //         error = 'Invalid user name/password';
+    //     }
+    // }
+    // catch(e) {
+    //     error = e.toString();
+    // }
+
+
+    if (login.toLowerCase() == 'test' && password == 'test') {
+        id = 1;
+        fn = 'TestFirst';
+        ln = 'TestLast';
+    }
+    else {
+        error = 'Invalid user name/password';
+    }
+
+    var ret = { id: id, firstName: fn, lastName: ln, error: error };
+    res.status(200).json(ret);
+});
+
+app.post('/api/createUser', async (req, res, next) => {
+    var error = '';
+    const { login, password, firstName, lastName, email } = req.body;
+    const newUser = {
+        Login: login, 
+        Password: password,
+        FirstName: firstName,
+        LastName: lastName,
+        Email: email
+    };
+
+    // TODO: Database Implementation
+    // try {
+    //     const db = client.db();
+    //     const result = db.collection('Users').insertOne(newUser);
+    // }
+    // catch (e) {
+    //     error = e.toString();
+    // }
+
+    var ret = { error: error };
+    res.status(200).json(ret);
+});
