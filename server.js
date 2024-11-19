fae2679226f8446e42dc9854529eb47922f32d85
parhamdevin <parhamd08@gmail.com>
2024-11-19 17:02:59 -0500

    added Collections Users and Stats

--- server.js
+++ server.js
@@ -6,26 +6,11 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, DELETE, OPTIONS'
-    );
-    next();
-});
-
-app.listen(5000);
-
-// TODO: Database Implementation
-// const MongoClient = require('mongodb').MongoClient;
-// const url = '';
-// const client = new MongoClient(url);
-// client.connect();
+//TODO: Database Implementation
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://dbAdmin:i79M5ezCQfyyYX@@processes.ysma6.mongodb.net/COP4331?retryWrites=true&w=majority&appName=Processes';
+const client = new MongoClient(url);
+client.connect();
 
 app.post('/api/login', async (req, res, next) => {
     var error = '';
@@ -35,22 +20,22 @@ app.post('/api/login', async (req, res, next) => {
     var ln = '';
 
     // TODO: Database Implementation
-    // try {
-    //     const db = client.db();
-    //     const results = await db.collection('Users').find({ Login: login, Password: password }).toArray();
+    try {
+        const db = client.db();
+        const results = await db.collection('Users').find({ Login: login, Password: password }).toArray();
 
-    //     if (results.length > 0) {
-    //         id = results[0].UserId;
-    //         fn = results[0].FirstName;
-    //         ln = results[0].LastName;
-    //     }
-    //     else {
-    //         error = 'Invalid user name/password';
-    //     }
-    // }
-    // catch(e) {
-    //     error = e.toString();
-    // }
+        if (results.length > 0) {
+            id = results[0].UserId;
+            fn = results[0].FirstName;
+            ln = results[0].LastName;
+        }
+        else {
+            error = 'Invalid user name/password';
+        }
+    }
+    catch(e) {
+        error = e.toString();
+    }
 
 
     if (login.toLowerCase() == 'test' && password == 'test') {
@@ -78,14 +63,29 @@ app.post('/api/createUser', async (req, res, next) => {
     };
 
     // TODO: Database Implementation
-    // try {
-    //     const db = client.db();
-    //     const result = db.collection('Users').insertOne(newUser);
-    // }
-    // catch (e) {
-    //     error = e.toString();
-    // }
+    try {
+        const db = client.db();
+        const result = db.collection('Users').insertOne(newUser);
+    }
+    catch (e) {
+        error = e.toString();
+    }
 
     var ret = { error: error };
     res.status(200).json(ret);
 });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
+
+app.listen(5000);
